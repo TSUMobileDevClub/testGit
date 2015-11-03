f394789b238173ff3d79c2c5e7f2bd5298d22930
antpowell <powell.anthony.e@gmail.com>
2015-11-02 23:28:53 -0600

    Need to add back button to CustomReg screen

--- MobileDevGroupCourse/practiceApp/app/src/main/java/com/example/sgt_powell/practiceapp/CustomRegistration.java
+++ MobileDevGroupCourse/practiceApp/app/src/main/java/com/example/sgt_powell/practiceapp/CustomRegistration.java
@@ -1,15 +1,17 @@
 package com.example.sgt_powell.practiceapp;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class CustomRegistration extends AppCompatActivity {
-    private Button newRegisterBtn;
     private TextView username;
     private TextView email;
     private TextView password;
@@ -20,6 +22,7 @@ public class CustomRegistration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_registration);
+//        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         username = (TextView) findViewById(R.id.reg_username_textfield);
         //lengthCheck(username.getText());
@@ -31,13 +34,37 @@ public class CustomRegistration extends AppCompatActivity {
         password_verified = (TextView) findViewById(R.id.reg_user_2nd_password);
         //passwordValidation(password.getText(), password_verified.getText());
 
-        newRegisterBtn = (Button) findViewById(R.id.regBtn);
+        Button newRegisterBtn = (Button) findViewById(R.id.reg_button);
         newRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 performChecks(username.getText(), email.getText(), password.getText(), password_verified.getText());
             }
         });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_none_default, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private boolean performChecks(CharSequence text, CharSequence email, CharSequence pass1, CharSequence pass2) {
+
+        return lengthCheck(text) && isEmail(email) && passwordValidation(pass1.toString(), pass2.toString());
 
 
     }
@@ -58,20 +85,15 @@ public class CustomRegistration extends AppCompatActivity {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
-    public boolean passwordValidation(CharSequence pass1, CharSequence pass2) {
-        if (!(pass1 == pass2)){
+    public boolean passwordValidation(String pass1, String pass2) {
+        if (!(pass1.equals(pass2))){
              Toast toast = Toast.makeText(this, "Passwords don't match.", Toast.LENGTH_LONG);
             toast.show();
         }
-        return pass1 == pass2;
+        return pass1.equals(pass2);
     }
 
 
-    protected boolean performChecks(CharSequence text, CharSequence email, CharSequence pass1, CharSequence pass2) {
 
-        return lengthCheck(text) && isEmail(email) && passwordValidation(pass1, pass2);
-
-
-}
 
 }
--- MobileDevGroupCourse/practiceApp/app/src/main/java/com/example/sgt_powell/practiceapp/MainActivity.java
+++ MobileDevGroupCourse/practiceApp/app/src/main/java/com/example/sgt_powell/practiceapp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.sgt_powell.practiceapp;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         signInBtn = (Button) findViewById(R.id.loginBtn);
         signInBtn.setOnClickListener(new View.OnClickListener() {
@@ -38,7 +42,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void signIn(){
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_sign_in, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.action_settings){
+            Intent intent = new Intent(this, CustomRegistration.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+        private void signIn(){
         //Alert Sign in
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Give us a sec while we sign you in.").setTitle("Signing In...").create().show();
